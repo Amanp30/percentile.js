76c063de0d9b6e56f73a03ef9e191d2f9ceaab77
Aman Pareek <107720919+Amanp30@users.noreply.github.com>
2022-12-05 13:20:15 +0530

    Add files via upload

---
+++ percentile.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+
+exports.cahnge = function (n1, n2, showpercent, lastdig = 2) {
+  var diff = n2 - n1;
+  var increase = (diff / n1) * 100;
+  if (showpercent) {
+    return Number(increase).toFixed(lastdig) + "%";
+  } else {
+    return Number(increase).toFixed(lastdig);
+  }
+};
+
+exports.not = function (n1, n2, showpercent, lastdig = 2) {
+  var increase = (n1 / n2) * 100;
+  if (showpercent) {
+    return Number(increase).toFixed(lastdig) + "%";
+  } else {
+    return Number(increase).toFixed(lastdig);
+  }
+};
+
+exports.percentoff = function (n1, n2, lastdig = 2) {
+  var increase = (n1 * n2) / 100;
+  return Number(increase).toFixed(lastdig);
+};
